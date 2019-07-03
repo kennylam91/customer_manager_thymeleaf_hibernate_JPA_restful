73046f5010765b11cb2f11429bfeab5d37b9be46
kennylam91 <phamvanlam1991@gmail.com>
2019-07-03 13:45:02 +0700

    show Create Customer form: OK

--- src/main/java/com/codegym/cms/controller/CustomerController.java
+++ src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -1,12 +1,19 @@
 package com.codegym.cms.controller;
 
+import com.codegym.cms.model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomerController {
     @GetMapping("/create-customer")
-    public String showCreateForm(){
-        return "create";
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
     }
+
 }
---
+++ src/main/java/com/codegym/cms/model/Customer.java
@@ -0,0 +1,24 @@
+package com.codegym.cms.model;
+
+public class Customer {
+    public Customer() {
+    }
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
