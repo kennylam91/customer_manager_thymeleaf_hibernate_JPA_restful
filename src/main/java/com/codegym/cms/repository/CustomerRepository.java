0e59f0882cc28867cd8303c8b642436c29e4b90b
kennylam91 <phamvanlam1991@gmail.com>
2019-07-11 10:27:49 +0700

    extends CrudRepository

--- src/main/java/com/codegym/cms/repository/CustomerRepository.java
+++ src/main/java/com/codegym/cms/repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.codegym.cms.repository;
 
 import com.codegym.cms.model.Customer;
+import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends Repository<Customer> {
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
 }
